311ece65125940cfe3d9fef8973f34e351a5da14
Pedro Echavarria <pedroechavarria@Pedros-MacBook-Pro.local>
2022-10-30 14:46:24 -0400

    test commit

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SparkLineComponent, Inject, SpaklineTooltip } from '@syncfusion/ej2-react-charts';
+
+const SparkLine = () => {
+    return (
+        <div>SparkLine</div>
+    )
+}
+
+export default SparkLine

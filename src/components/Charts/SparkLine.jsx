29481eb703d57a8bc5f738e5b4e295952b221a45
Pedro Echavarria <pechavarria62@gmail.com>
2023-11-29 20:21:24 -0600

    added new charts one is not working so Ill work on that later

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { SparkLineComponent, Inject, SpaklineTooltip } from '@syncfusion/ej2-react-charts';
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip} from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
-    return (
-        <div>SparkLine</div>
-    )
-}
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={width}
+			lineWidth={1}
+			valueType="Numeric"
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			dataSource={data}
+			xName="x"
+			yName="y"
+			type={type}
+			tooltipSettings={{
+				visible: true,
+				format: '${x} : data ${y}',
+				trackLineSettings: {
+					visible: true
+				},
+			}}
+		>
+			<Inject services={[SparklineTooltip]} />
+			<p>coñon</p>
+		</SparklineComponent>
+	);
+};
 
-export default SparkLine
+export default SparkLine;
